7e6c27ea8eecc77b5c3ff8595cbf3f46daf728ff
Bella <83490314+bellatrix-ds@users.noreply.github.com>
2026-01-11 19:19:40 +0300

    Update Lending_dashboard.py

--- 04_DeFi_Lending_Protocol/Lending_dashboard.py
+++ 04_DeFi_Lending_Protocol/Lending_dashboard.py
@@ -34,9 +34,10 @@ st.markdown("""
     color: white;
 ">
     <h4 style='margin-bottom: 5px;'>📌 Overview</h4>
-    <p style='color: #b0b0b0; margin-top: 0;'>This dashboard gives a clear snapshot of the DeFi lending market. It shows where liquidity is concentrated, how it changes over time, and which assets offer yield.Use the time and chain filters to focus on specific markets or periods.
-Overall, this dashboard helps you quickly understand liquidity trends, protocol dominance, and yield opportunities in DeFi lending.
-You can filter by chain and select a time period (last 3, 6, or 12 months).</p>
+    <p style='color: #b0b0b0; margin-top: 0;'>This dashboard gives a clear snapshot of the DeFi lending market. 
+    - It shows where liquidity is concentrated, how it changes over time, and which assets offer yield.
+    - Use the time and chain filters to focus on specific markets or periods.
+    - You can filter by chain and select a time period (last 3, 6, or 12 months).</p>
 </div>
 """, unsafe_allow_html=True)
 

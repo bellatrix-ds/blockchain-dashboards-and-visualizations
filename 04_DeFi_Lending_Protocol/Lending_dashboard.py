9993fe32cdbe3d0bafde54a53c6dcde3314dcb06
Bella <83490314+bellatrix-ds@users.noreply.github.com>
2026-01-11 22:51:51 +0300

    Update Lending_dashboard.py

--- 04_DeFi_Lending_Protocol/Lending_dashboard.py
+++ 04_DeFi_Lending_Protocol/Lending_dashboard.py
@@ -21,7 +21,7 @@ df_yield = pd.read_csv("https://raw.githubusercontent.com/bellatrix-ds/blockchai
 # -----------------------------
 st.set_page_config(layout="wide")
 st.title("🧮 DeFi Lending Protocol Dashboard")
-st.subtitle("Liquidity, TVL, and Yield Across Top 4 Lending Protocols")
+st.subheader("Liquidity, TVL, and Yield Across Top 4 Lending Protocols")
 
 # -----------------------------
 # Dashboard Header
